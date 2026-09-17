4121843b6e0c6cea482e72454e4fe8ba8d1b75c2
ngocduyprc2006-tech <ngocduyprc2006@gmail.com>
2026-09-17 13:28:23 +0700

    login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+// Hàm kiểm tra đăng nhập
+function checkLogin(user, pass) {
+    if (user === 'admin' && pass === '123') {
+        return true;
+    }
+    return false;
+}
+
+// Xuất hàm để có thể test bằng thư viện Jest (Node.js)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = checkLogin;
+}
